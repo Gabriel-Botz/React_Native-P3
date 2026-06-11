abd9217a96641ad1a347daa92e88002a87cab76a
Gabriel Botelho <gabriel.c.botelho@hotmail.com>
2026-06-11 20:15:45 -0300

    estilização componente

--- src/components/InputField/InputField.styles.ts
+++ src/components/InputField/InputField.styles.ts
@@ -1,17 +1,20 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
+
   container: {
-    borderWidth: 1,
+    borderWidth: 1, 
     borderColor: '#ccc',
     borderRadius: 8,
     paddingHorizontal: 12,
     marginBottom: 12,
     height: 50,
   },
+
   input: {
     flex: 1,
     fontSize: 16,
     color: '#000',
   },
+
 });
